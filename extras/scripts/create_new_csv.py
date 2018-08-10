58598c20bc7c91d56391ed0b5c1e3c383c8d7eb8
Daroface <dominik1050@gmail.com>
2018-08-10 22:16:20 +0200

    Changed path to downloaded images.

--- extras/scripts/create_new_csv.py
+++ extras/scripts/create_new_csv.py
@@ -239,9 +239,9 @@ def main():
     #Ustaw sieżkę u siebie lokalnie do repo###
     file_path = r"sciezka/do/repo/extras/"
 
-    train_path = file_path + r"images/"
-    valid_path = file_path + r"images/"
-    test_path = file_path + r"images/"
+    train_path = r"sciezka/do/repo/images/"
+    valid_path = r"sciezka/do/repo/images/"
+    test_path = r"sciezka/do/repo/images/"
 
     oid_train_bboxes_file = file_path + r"oid/train-annotations-bbox.csv"
     oid_train_labels_file = file_path + r"oid/labels/train-annotations-human-imagelabels-boxable.csv"
